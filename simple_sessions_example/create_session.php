59f1801cb5719a1cbbbe3c00689bb61a52b5f994
Prajwal Bagewadi <77821176+prajwalbagewadi@users.noreply.github.com>
2024-03-12 10:18:45 +0530

    Add files via upload

---
+++ simple_sessions_example/create_session.php
@@ -0,0 +1,9 @@
+<?php
+// CREATE A SESSION
+session_start();
+$_SESSION['u_id']=181;
+$_SESSION['u_name']="prajwal";
+?>
+<html>
+<a href="sess.php">click</a>">
+<html>
---
+++ simple_sessions_example/sess.php
@@ -0,0 +1,10 @@
+<?php
+session_start();
+$uid=$_SESSION['u_id'];
+$u_name=$_SESSION['u_name'];
+echo "uname:".$u_name."<br>";
+echo "uid:".$uid."<br>";
+?>
+<html>
+<a href="kill.php">click</a>">
+<html>
